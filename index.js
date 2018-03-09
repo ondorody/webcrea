3bfe5ca8bceb4d1615e22c4c96a1be9d5d1c0e65
ondorody <37157793+ondorody@users.noreply.github.com>
2018-03-09 10:19:23 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,187 @@
+﻿// El Mehdi LAIDOUNI
+
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('request')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+// Process urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process json
+app.use(bodyParser.json())
+
+// Index route
+app.get('/', function (req, res) {
+    res.send('Bonjour, Je suis votre Facebook Messenger Bot')
+})
+
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'Bot_Messenger_App') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+})
+
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
+
+
+// End Point
+
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            if (text === 'Menu') {
+                sendGenericMessage(sender)
+                continue
+            }
+            sendTextMessage(sender, "Bot: " + text.substring(0, 200))
+        }
+        if (event.postback) {
+            text = JSON.stringify(event.postback)
+            sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+            continue
+        }
+    }
+    res.sendStatus(200)
+})
+
+var token = "EAAR7rXLj81wBAEJmS62ZBE5stLHoeU0utxZAPnINOtXINLk6y2qvPprPSr24PYky5295bsNezPMIvF8xVIlGPQ0ZACQhiAbKt6MlzUZBoiZAE18bZBagDjzfXfZCPuv5Gylaaxzmp4MDm4wjdWRnupkcfqTjfh35AwKZA785ERJfVAZDZD"
+
+// Echo back messages
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+
+// Two cards.
+
+function sendGenericMessage(sender) {
+    messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "GAB disponible",
+                    "subtitle": "Consultez",
+                    "image_url": "https://cdn.pixabay.com/photo/2016/07/17/22/06/atm-1524870_960_720.jpg",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://www.google.bf/search?rlz=1C1NHXL_frBF779BF779&biw=1600&bih=794&q=GAB+CORIS+BANQUE&npsic=0&rflfq=1&rlha=0&rllag=12352601,-1505980,2159&tbm=lcl&ved=0ahUKEwjls837-N7ZAhUDwFkKHRFwDxgQjGoIWQ&tbs=lrf:!2m4!1e17!4m2!17m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:3&rldoc=1#rlfi=hd:;si:;mv:!1m3!1d20786.132230519084!2d-1.5254232!3d12.356123!2m3!1f0!2f0!3f0!3m2!1i599!2i256!4f13.1;tbs:lrf:!2m1!1e3!2m4!1e17!4m2!17m1!1e2!3sIAE,lf:1,lf_ui:3",
+                        "title": "Coris Bank"
+                    }, {
+                        "type": "web_url",
+                        "url": "https://www.google.bf/search?rlz=1C1NHXL_frBF779BF779&biw=1600&bih=794&q=GAB+UBA&npsic=0&rflfq=1&rlha=0&rllag=12349389,-1513259,1458&tbm=lcl&ved=0ahUKEwjsqdSW-d7ZAhVLq1kKHYr_Ah8QjGoITQ&tbs=lrf:!2m4!1e17!4m2!17m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:3&rldoc=1#rlfi=hd:;si:;mv:!1m3!1d20055.420460186306!2d-1.5111939!3d12.355461900000002!2m3!1f0!2f0!3f0!3m2!1i348!2i494!4f13.1;tbs:lrf:!2m1!1e3!2m4!1e17!4m2!17m1!1e2!3sIAE,lf:1,lf_ui:3",
+                        "title": "UBA"
+                    },{
+                        "type": "web_url",
+                        "url": "https://www.google.bf/search?rlz=1C1NHXL_frBF779BF779&biw=1600&bih=794&q=GAB+ECOBANK&npsic=0&rflfq=1&rlha=0&rllag=12362415,-1481037,1731&tbm=lcl&ved=0ahUKEwiYq7PA-d7ZAhUuwlkKHcxNCRUQjGoITg&tbs=lrf:!2m4!1e17!4m2!17m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:4&rldoc=1#rlfi=hd:;si:;mv:!1m3!1d23222.20425482326!2d-1.4940682!3d12.353902249999999!2m3!1f0!2f0!3f0!3m2!1i289!2i286!4f13.1;tbs:lrf:!2m1!1e3!2m4!1e17!4m2!17m1!1e2!3sIAE,lf:1,lf_ui:4",
+                        "title": "Ecobank"
+                    }],
+                }, {
+                    "title": "Reservez un ticket",
+                    "subtitle": "Choisir sa banque",
+                    "image_url": "https://cdn.pixabay.com/photo/2016/12/13/17/18/fax-1904644_960_720.jpg",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Coris Bank",
+                        "payload": "Ticket N°1",
+                    },{
+                        "type": "postback",
+                        "title": "UBA",
+                        "payload": "Ticket N°2",
+                    }, {
+                        "type": "postback",
+                        "title": "Ecobank",
+                        "payload": "Ticket N°3",
+                    }],
+                },  {
+                        "title": "Produits ",
+                    "subtitle": " Telechargeable ",
+                    "image_url": "https://cdn.pixabay.com/photo/2015/11/23/10/52/ec-1058106_960_720.png",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://play.google.com/store/apps/details?id=com.app.ecobank&hl=fr",
+                        "title": "Ecobank Mobile Banking", 
+                        
+                    },{
+                        "type": "web_url",
+                        "url": "https://play.google.com/store/apps/details?id=com.lamedis.boursify.corisbourse&hl=fr",
+                        "title": " Coris Bourse",
+                    }, {
+                        "type": "web_url",
+                        "url": "https://play.google.com/store/apps/details?id=com.uba.vericash&hl=fr ",
+                        "title": "UBA Mobile Banking",
+                    }],
+                }, {
+                    "title": "Comparateur de Prix",
+                    "subtitle": "versus",
+                    "image_url": "http://img.over-blog-kiwi.com/1/53/19/45/20160510/ob_15306e_comparateur-legende.png",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Coris Bank",
+                        "payload": "Ouverture d'un compte epargne à partir de 10.000 fcfa",
+                    }, {
+                        "type": "postback",
+                        "title": "SGBF",
+                        "payload": "Ouvrir un compte courant est totalement gratuite à #SGBF",
+                    }, {
+                        "type": "postback",
+                        "title": "UBA",
+                        "payload": "Ouverture d'un compte epargne à partir de 10.000 fcfa",
+                    }],
+                }]  
+            } 
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+var request = require('request');
+
+request('https://iptoearth.expeditedaddons.com/?api_key=HTBCSM05UX6Q07389DL51JPN2FAVZ847EY3OR2G6WI1K94&ip=68.10.149.45', function (error, response, body) {
+    console.log('Status:', response.statusCode);
+    console.log('Headers:', JSON.stringify(response.headers));
+    console.log('Response:', body);
+});
