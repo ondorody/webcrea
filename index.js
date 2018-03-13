4972768bcba756318c86420f3b2bc6326f04e034
ondorody <37157793+ondorody@users.noreply.github.com>
2018-03-13 17:54:12 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -188,12 +188,39 @@ request('https://iptoearth.expeditedaddons.com/?api_key=HTBCSM05UX6Q07389DL51JPN
     console.log('Response:', body);
 });
 
-https://graph.facebook.com/v2.6/me/messages?access_token=<EAAR7rXLj81wBAPVadWFhRWqtKUDD68eKUWJ1ef2iAEgqvbedZBkJTa4ZBdkY97ZAjoX4otsxTZCwIh3bUHDtaf2bzlbDNa8QKl1jTevPkEhZC7ilLaIZCCF25nC8mW4T8RsHQ3ATYUbIQM24tYhjq31yQaK6NahqcdBN8tzi67ZBwZDZD>
+let message = {
+    "attachment": {
+        "type": "template",
+        "payload": {
+            "template_type": "generic",
+            "elements": [{
+                "title": "I took Peter's 'Which Hat Are You?' Quiz",
+                "image_url": "https://bot.peters-hats.com/img/hats/fez.jpg",
+                "subtitle": "My result: Fez",
+                "default_action": {
+                    "type": "web_url",
+                    "url": "https://bot.peters-hats.com/view_quiz_results.php?user=24601"
+                },
+                "buttons": [{
+                    "type": "web_url",
+                    "url": "https://bot.peters-hats.com/hatquiz.php?referer=24601",
+                    "title": "Take the Quiz"
+                }]
+            }]
+        }
+    }
+};
 
-"messaging_type"; "<RESPONSE>"
-"recipient";{
-    "id";"<PSID>"
-}
-"message";{
-    "text";"hello, world!"
-}
+MessengerExtensions.beginShareFlow(function (share_response) {
+    // User dismissed without error, but did they share the message?
+    if (share_response.is_sent) {
+        // The user actually did share. 
+        // Perhaps close the window w/ requestCloseBrowser().
+    }
+},
+    function (errorCode, errorMessage) {
+        // An error occurred in the process
+
+    },
+    message,
+    "broadcast");
