926590e5ef5773d76cf351a80361e562745b5fad
ondorody <37157793+ondorody@users.noreply.github.com>
2018-03-13 16:24:17 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -157,29 +157,8 @@ function sendGenericMessage(sender) {
                             "type": "postback",
                             "title": "Prérequis  d'ouverture d'un compte bancaire",
                             "payload": "Coris Bank: A -- SGBF: B -- UBA: C",
-                        },{
-                        "title": "Welcome to Peter'\''s Hats",
-                        "image_url": "https://petersfancybrownhats.com/company_image.png",
-                        "subtitle": "We'\''ve got the right hat for everyone.",
-                        "default_action": {
-                            "type": "web_url",
-                            "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
-                            "messenger_extensions": true,
-                            "webview_height_ratio": "tall",
-                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                        },
-                        "buttons": [
-                            {
-                                "type": "web_url",
-                                "url": "https://petersfancybrownhats.com",
-                                "title": "View Website"
-                            }, {
-                                "type": "postback",
-                                "title": "Start Chatting",
-                                "payload": "DEVELOPER_DEFINED_PAYLOAD"
-                            }
-                        ]
-
+                    },{
+                    
                     }],
                 }]
             }
