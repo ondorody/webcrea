066d6bdef42d6ae504eadc722ec22d05f87139bf
ondorody <37157793+ondorody@users.noreply.github.com>
2018-03-13 02:36:49 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -185,158 +185,35 @@ request('https://iptoearth.expeditedaddons.com/?api_key=HTBCSM05UX6Q07389DL51JPN
     console.log('Headers:', JSON.stringify(response.headers));
     console.log('Response:', body);
 });
-var googleMapsClient = require('@google/maps').createClient({
-    key: 'AIzaSyARj7YbiKz5v77ZzurXMDKyoRHCfqUKg34'
-});
-// Geocode an address.
-googleMapsClient.geocode({
-    address: '1600 Amphitheatre Parkway, Mountain View, CA'
-}, function (err, response) {
-    if (!err) {
-        console.log(response.json.results);
-    }
-    });
-var map;
 
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 45.518, lng: -122.672 },
-        zoom: 18,
-        mapTypeId: 'satellite',
-        heading: 90,
-        tilt: 45
-    });
-}
-
-function rotate90() {
-    var heading = map.getHeading() || 0;
-    map.setHeading(heading + 90);
-}
-
-function autoRotate() {
-    // Determine if we're showing aerial imagery.
-    if (map.getTilt() !== 0) {
-        window.setInterval(rotate90, 3000);
-    }
-}
-// This example defines an image map type using the Gall-Peters
-// projection.
-// https://en.wikipedia.org/wiki/Gall%E2%80%93Peters_projection
-
-function initMap() {
-    // Create a map. Use the Gall-Peters map type.
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 0,
-        center: { lat: 0, lng: 0 },
-        mapTypeControl: false
-    });
-
-    initGallPeters();
-    map.mapTypes.set('gallPeters', gallPetersMapType);
-    map.setMapTypeId('gallPeters');
-
-    // Show the lat and lng under the mouse cursor.
-    var coordsDiv = document.getElementById('coords');
-    map.controls[google.maps.ControlPosition.TOP_CENTER].push(coordsDiv);
-    map.addListener('mousemove', function (event) {
-        coordsDiv.textContent =
-            'lat: ' + Math.round(event.latLng.lat()) + ', ' +
-            'lng: ' + Math.round(event.latLng.lng());
-    });
-
-    // Add some markers to the map.
-    map.data.setStyle(function (feature) {
-        return {
-            title: feature.getProperty('name'),
-            optimized: false
-        };
-    });
-    map.data.addGeoJson(cities);
-}
-
-var gallPetersMapType;
-function initGallPeters() {
-    var GALL_PETERS_RANGE_X = 800;
-    var GALL_PETERS_RANGE_Y = 512;
-
-    // Fetch Gall-Peters tiles stored locally on our server.
-    gallPetersMapType = new google.maps.ImageMapType({
-        getTileUrl: function (coord, zoom) {
-            var scale = 1 << zoom;
-
-            // Wrap tiles horizontally.
-            var x = ((coord.x % scale) + scale) % scale;
-
-            // Don't wrap tiles vertically.
-            var y = coord.y;
-            if (y < 0 || y >= scale) return null;
-
-            return 'https://developers.google.com/maps/documentation/' +
-                'javascript/examples/full/images/gall-peters_' + zoom +
-                '_' + x + '_' + y + '.png';
-        },
-        tileSize: new google.maps.Size(GALL_PETERS_RANGE_X, GALL_PETERS_RANGE_Y),
-        isPng: true,
-        minZoom: 0,
-        maxZoom: 1,
-        name: 'Gall-Peters'
-    });
-
-    // Describe the Gall-Peters projection used by these tiles.
-    gallPetersMapType.projection = {
-        fromLatLngToPoint: function (latLng) {
-            var latRadians = latLng.lat() * Math.PI / 180;
-            return new google.maps.Point(
-                GALL_PETERS_RANGE_X * (0.5 + latLng.lng() / 360),
-                GALL_PETERS_RANGE_Y * (0.5 - 0.5 * Math.sin(latRadians)));
-        },
-        fromPointToLatLng: function (point, noWrap) {
-            var x = point.x / GALL_PETERS_RANGE_X;
-            var y = Math.max(0, Math.min(1, point.y / GALL_PETERS_RANGE_Y));
-
-            return new google.maps.LatLng(
-                Math.asin(1 - 2 * y) * 180 / Math.PI,
-                -180 + 360 * x,
-                noWrap);
-        }
-    };
-}
-
-// GeoJSON, describing the locations and names of some cities.
-var cities = {
-    type: 'FeatureCollection',
-    features: [{
-        type: 'Feature',
-        geometry: { type: 'Point', coordinates: [-87.650, 41.850] },
-        properties: { name: 'Chicago' }
-    }, {
-        type: 'Feature',
-        geometry: { type: 'Point', coordinates: [-149.900, 61.218] },
-        properties: { name: 'Anchorage' }
-    }, {
-        type: 'Feature',
-        geometry: { type: 'Point', coordinates: [-99.127, 19.427] },
-        properties: { name: 'Mexico City' }
-    }, {
-        type: 'Feature',
-        geometry: { type: 'Point', coordinates: [-0.126, 51.500] },
-        properties: { name: 'London' }
-    }, {
-        type: 'Feature',
-        geometry: { type: 'Point', coordinates: [28.045, -26.201] },
-        properties: { name: 'Johannesburg' }
-    }, {
-        type: 'Feature',
-        geometry: { type: 'Point', coordinates: [15.322, -4.325] },
-        properties: { name: 'Kinshasa' }
-    }, {
-        type: 'Feature',
-        geometry: { type: 'Point', coordinates: [151.207, -33.867] },
-        properties: { name: 'Sydney' }
-    }, {
-        type: 'Feature',
-        geometry: { type: 'Point', coordinates: [0, 0] },
-        properties: { name: '0°N 0°E' }
-    }]
-
-   <HTMLAllCollection> < iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/search?q=BANQUE&key=AIzaSyBYZQDnBdvoOwUmv9o_eXmd0oYypPvc-7s" allowfullscreen></iframe>
+<!DOCTYPE html>
+    <html>
+        <head>
+            <style>
+                #map {
+                    height: 400px;
+        width: 100%;
+       }
+    </style>
+        </head>
+        <body>
+            <h3>My Google Maps Demo</h3>
+            <div id="map"></div>
+            <script>
+                function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+                    position: uluru,
+          map: map
+        });
+      }
+    </script>
+            <script async defer
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyARj7YbiKz5v77ZzurXMDKyoRHCfqUKg34&callback=initMap">
+            </script>
+        </body>
+    </html>
