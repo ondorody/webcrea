63f650117e45edfab834b4639f7a2825dee89f23
ondorody <37157793+ondorody@users.noreply.github.com>
2018-03-13 19:15:46 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -42,6 +42,9 @@ app.post('/webhook/', function (req, res) {
             if (text === 'Menu' || text === 'Demarrer' || text === 'Bank' || text === 'GAB') {
                 sendGenericMessage(sender)
                 continue
+            if (text === 'Carte' || text === 'Map' || text === 'Start' || text === 'Go') {
+                sendGenericMessaoge(sender)
+                continue
             }
             sendTextMessage(sender, "Bot: " + text.substring(0, 200))
         }
@@ -192,27 +195,7 @@ request('https://iptoearth.expeditedaddons.com/?api_key=HTBCSM05UX6Q07389DL51JPN
 //zone test 
 // End Point 2
 
-app.post('/webhook/', function (req, res) {
-    messaging_events = req.body.entry[0].messaging
-    for (i = 0; i < messaging_events.length; i++) {
-        event = req.body.entry[0].messaging[i]
-        sender = event.sender.id
-        if (event.message && event.message.text) {
-            text = event.message.text
-            if (text === 'Menu' || text === 'Demarrer' || text === 'Bank' || text === 'GAB') {
-                sendGenericMessaoge(sender)
-                continue
-            }
-            sendTextMessage(sender, "Bot: " + text.substring(0, 200))
-        }
-        if (event.postback) {
-            text = JSON.stringify(event.postback)
-            sendTextMessage(sender, "Postback received: " + text.substring(0, 200), token)
-            continue
-        }
-    }
-    res.sendStatus(200)
-})
+
 
 function sendTextMessaoge(sender, text) {
     let messageData = { text: text }
