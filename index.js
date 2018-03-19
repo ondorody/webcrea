7b9b46ac42a936a444366cf2c342ceaef6fcf791
ondorody <37157793+ondorody@users.noreply.github.com>
2018-03-19 21:40:04 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -301,28 +301,3 @@ function facebookThreadAPI(jsonFile, cmd) {
         });
 }
 
-app.post('/webhook', function (req, res) {
-var data = req.body;
-
-// Make sure this is a page subscription
-if (data.object === 'page') {
-
-    // Iterate over each entry - there may be multiple if batched
-    data.entry.forEach(function(entry) {
-    var pageID = entry.id;
-    var timeOfEvent = entry.time;
-
-    // Iterate over each messaging event
-    entry.messaging.forEach(function(event) {
-        if (event.message) {
-        receivedMessage(event);
-        } else {
-        console.log("Webhook received unknown event: ", event);
-        }
-    });
-    });
-
-    res.sendStatus(200);
-}
-});
-
