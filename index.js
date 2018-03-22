10ba78960d38b3b2a4fd68d66ced14554e8e2b4e
ondorody <37157793+ondorody@users.noreply.github.com>
2018-03-22 19:29:02 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -300,6 +300,44 @@ function sendGenericMessaoge(sender) {
 
 
 
+// Server index page
+app.get("/", function (req, res) {
+    res.send("Deployed!");
+});
+
+// Facebook Webhook
+// Used for verification
+app.get("/webhook", function (req, res) {
+    if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
+        console.log("Verified webhook");
+        res.status(200).send(req.query["hub.challenge"]);
+    } else {
+        console.error("Verification failed. The tokens do not match.");
+        res.sendStatus(403);
+    }
+});
+
+// All callbacks for Messenger will be POST-ed here
+app.post("/webhook", function (req, res) {
+    // Make sure this is a page subscription
+    if (req.body.object == "page") {
+        // Iterate over each entry
+        // There may be multiple entries if batched
+        req.body.entry.forEach(function (entry) {
+            // Iterate over each messaging event
+            entry.messaging.forEach(function (event) {
+                if (event.postback) {
+                    processPostback(event);
+                } else if (event.message) {
+                    processMessage(event);
+                }
+            });
+        });
+
+        res.sendStatus(200);
+    }
+});
+
 function processPostback(event) {
     var senderId = event.sender.id;
     var payload = event.postback.payload;
