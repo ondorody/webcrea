e7acca01e1c6fd4de0c02d7db2991096432dafde
ondorody <37157793+ondorody@users.noreply.github.com>
2018-03-22 19:06:44 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -296,127 +296,211 @@ function sendGenericMessaoge(sender) {
 }
 
 
-        
+  // teste mesage welcome
 
+var express = require("express");
+var request = require("request");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
+var db = mongoose.connect(process.env.MONGODB_URI);
+var Movie = require("./models/movie");
 
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.listen((process.env.PORT || 5000));
 
+// Server index page
+app.get("/", function (req, res) {
+    res.send("Deployed!");
+});
 
-// text pour message d'accueil 
+// Facebook Webhook
+// Used for verification
+app.get("/webhook", function (req, res) {
+    if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
+        console.log("Verified webhook");
+        res.status(200).send(req.query["hub.challenge"]);
+    } else {
+        console.error("Verification failed. The tokens do not match.");
+        res.sendStatus(403);
+    }
+});
 
-
-function setupGreetingText(res) {
-    var messageData = {
-        "greeting": [
-            {
-                "locale": "default",
-                "text": "Greeting text for default local !"
-            }, {
-                "locale": "en_FR",
-                "text": "Bonjour et bienvenue !"
-            }
-        ]
-    };
-    request({
-        url: "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAR7rXLj81wBAL2s4XGAxv0pjVbKKrZAG7qANFGKvvNC5k8qLWdf907PZBu1KNBldiccK5LlY8zSbuorboH25VnsgSDbKTelpjVc74lAOvmazcSXVzU7GrkGcYl8ZBGHZBMPSdez1AWmgNcttq3J7GifmWI8vI7KVZAYvDF1IIwZDZD",
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        form: messageData
-    },
-        function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                // Print out the response body
-                res.send(body);
-
-            } else {
-                // TODO: Handle errors
-                res.send(body);
-            }
-        });
-
-}
-
-function setupPersistentMenu(res) {
-    var messageData =
-        {
-            "persistent_menu": [
-                {
-                    "locale": "default",
-                    "composer_input_disabled": true,
-                    "call_to_actions": [
-                        {
-                            "title": "Info",
-                            "type": "nested",
-                            "call_to_actions": [
-                                {
-                                    "title": "Help",
-                                    "type": "postback",
-                                    "payload": "HELP_PAYLOAD"
-                                },
-                                {
-                                    "title": "Contact Me",
-                                    "type": "postback",
-                                    "payload": "CONTACT_INFO_PAYLOAD"
-                                }
-                            ]
-                        },
-                        {
-                            "type": "web_url",
-                            "title": "Visit website ",
-                            "url": "http://www.techiediaries.com",
-                            "webview_height_ratio": "full"
-                        }
-                    ]
-                },
-                {
-                    "locale": "zh_CN",
-                    "composer_input_disabled": false
+// All callbacks for Messenger will be POST-ed here
+app.post("/webhook", function (req, res) {
+    // Make sure this is a page subscription
+    if (req.body.object == "page") {
+        // Iterate over each entry
+        // There may be multiple entries if batched
+        req.body.entry.forEach(function (entry) {
+            // Iterate over each messaging event
+            entry.messaging.forEach(function (event) {
+                if (event.postback) {
+                    processPostback(event);
+                } else if (event.message) {
+                    processMessage(event);
                 }
-            ]
-        };
-    // Start the request
-    request({
-        url: "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAR7rXLj81wBAL2s4XGAxv0pjVbKKrZAG7qANFGKvvNC5k8qLWdf907PZBu1KNBldiccK5LlY8zSbuorboH25VnsgSDbKTelpjVc74lAOvmazcSXVzU7GrkGcYl8ZBGHZBMPSdez1AWmgNcttq3J7GifmWI8vI7KVZAYvDF1IIwZDZD",
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        form: messageData
-    },
-        function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                // Print out the response body
-                res.send(body);
-
-            } else {
-                // TODO: Handle errors
-                res.send(body);
-            }
+            });
         });
 
+        res.sendStatus(200);
+    }
+});
+
+function processPostback(event) {
+    var senderId = event.sender.id;
+    var payload = event.postback.payload;
+
+    if (payload === "Greeting") {
+        // Get user's first name from the User Profile API
+        // and include it in the greeting
+        request({
+            url: "https://graph.facebook.com/v2.6/" + senderId,
+            qs: {
+                access_token: process.env.PAGE_ACCESS_TOKEN,
+                fields: "first_name"
+            },
+            method: "GET"
+        }, function (error, response, body) {
+            var greeting = "";
+            if (error) {
+                console.log("Error getting user's name: " + error);
+            } else {
+                var bodyObj = JSON.parse(body);
+                name = bodyObj.first_name;
+                greeting = "Hi " + name + ". ";
+            }
+            var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
+            sendMessage(senderId, { text: message });
+        });
+    } else if (payload === "Correct") {
+        sendMessage(senderId, { text: "Awesome! What would you like to find out? Enter 'plot', 'date', 'runtime', 'director', 'cast' or 'rating' for the various details." });
+    } else if (payload === "Incorrect") {
+        sendMessage(senderId, { text: "Oops! Sorry about that. Try using the exact title of the movie" });
+    }
 }
 
+function processMessage(event) {
+    if (!event.message.is_echo) {
+        var message = event.message;
+        var senderId = event.sender.id;
 
-function setupGetStartedButton(res) {
-    var messageData = {
-        "get_started": {
-            "payload": "getstarted"
+        console.log("Received message from senderId: " + senderId);
+        console.log("Message is: " + JSON.stringify(message));
+
+        // You may get a text or attachment but not both
+        if (message.text) {
+            var formattedMsg = message.text.toLowerCase().trim();
+
+            // If we receive a text message, check to see if it matches any special
+            // keywords and send back the corresponding movie detail.
+            // Otherwise search for new movie.
+            switch (formattedMsg) {
+                case "plot":
+                case "date":
+                case "runtime":
+                case "director":
+                case "cast":
+                case "rating":
+                    getMovieDetail(senderId, formattedMsg);
+                    break;
+
+                default:
+                    findMovie(senderId, formattedMsg);
+            }
+        } else if (message.attachments) {
+            sendMessage(senderId, { text: "Sorry, I don't understand your request." });
         }
-    };
-    // Start the request
-    request({
-        url: "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAR7rXLj81wBAL2s4XGAxv0pjVbKKrZAG7qANFGKvvNC5k8qLWdf907PZBu1KNBldiccK5LlY8zSbuorboH25VnsgSDbKTelpjVc74lAOvmazcSXVzU7GrkGcYl8ZBGHZBMPSdez1AWmgNcttq3J7GifmWI8vI7KVZAYvDF1IIwZDZD",
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        form: messageData
-    },
-        function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                // Print out the response body
-                res.send(body);
-
-            } else {
-                // TODO: Handle errors
-                res.send(body);
-            }
-        });
+    }
 }
+
+function findMovie(userId, movieTitle) {
+    request("http://www.omdbapi.com/?type=movie&t=" + movieTitle, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var movieObj = JSON.parse(body);
+            if (movieObj.Response === "True") {
+                var query = { user_id: userId };
+                var update = {
+                    user_id: userId,
+                    title: movieObj.Title,
+                    plot: movieObj.Plot,
+                    date: movieObj.Released,
+                    runtime: movieObj.Runtime,
+                    director: movieObj.Director,
+                    cast: movieObj.Actors,
+                    rating: movieObj.imdbRating,
+                    poster_url: movieObj.Poster
+                };
+                var options = { upsert: true };
+                Movie.findOneAndUpdate(query, update, options, function (err, mov) {
+                    if (err) {
+                        console.log("Database error: " + err);
+                    } else {
+                        message = {
+                            attachment: {
+                                type: "template",
+                                payload: {
+                                    template_type: "generic",
+                                    elements: [{
+                                        title: movieObj.Title,
+                                        subtitle: "Is this the movie you are looking for?",
+                                        image_url: movieObj.Poster === "N/A" ? "http://placehold.it/350x150" : movieObj.Poster,
+                                        buttons: [{
+                                            type: "postback",
+                                            title: "Yes",
+                                            payload: "Correct"
+                                        }, {
+                                            type: "postback",
+                                            title: "No",
+                                            payload: "Incorrect"
+                                        }]
+                                    }]
+                                }
+                            }
+                        };
+                        sendMessage(userId, message);
+                    }
+                });
+            } else {
+                console.log(movieObj.Error);
+                sendMessage(userId, { text: movieObj.Error });
+            }
+        } else {
+            sendMessage(userId, { text: "Something went wrong. Try again." });
+        }
+    });
+}
+
+function getMovieDetail(userId, field) {
+    Movie.findOne({ user_id: userId }, function (err, movie) {
+        if (err) {
+            sendMessage(userId, { text: "Something went wrong. Try again" });
+        } else {
+            sendMessage(userId, { text: movie[field] });
+        }
+    });
+}
+
+// sends message to user
+function sendMessage(recipientId, message) {
+    request({
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+        method: "POST",
+        json: {
+            recipient: { id: recipientId },
+            message: message,
+        }
+    }, function (error, response, body) {
+        if (error) {
+            console.log("Error sending message: " + response.error);
+        }
+    });
+}
+
+
+
+
 
