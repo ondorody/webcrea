46a3abbc3e11db4d4ec6621b363a1e9250764cb4
ondorody <37157793+ondorody@users.noreply.github.com>
2018-03-22 17:21:20 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -420,24 +420,7 @@ function setupGetStartedButton(res) {
         });
 }
 
-// autre tentative de gretting
-
-let request = require('request');
-
-request.post({
-    method: 'POST',
-    uri: `https://graph.facebook.com/v2.6/me/thread_settings?access_token=${ACCESS_TOKEN}`,
-    qs: {
-        setting_type: 'call_to_actions',
-        thread_state: 'new_thread',
-        call_to_actions: [{
-            payload: 'GET_START'
-        }]
-    },
-    json: true
-}, (err, res, body) => {
-    // Deal with the response
-    });
+// autre tentative de message de bienvenu
 
 request({
     method: 'POST',
