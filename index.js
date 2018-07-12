9dca92552b60cdf0902c3adc4dc9f335938d7171
ondorody <37157793+ondorody@users.noreply.github.com>
2018-07-12 21:03:26 +0000

    Update index.js

--- index.js
+++ index.js
@@ -72,27 +72,6 @@ app.post('/webhook/', function (req, res) {
         }
     }
     res.sendStatus(200)
-    
-    
-    messaging_events = req.body.entry[0].messaging
-    for (i = 0; i < messaging_events.length; i++) {
-        event = req.body.entry[0].messaging[i]
-        sender = event.sender.id
-        if (event.message && event.message.text) {
-            text = event.message.text
-            if (text === 'A' || text === 'B' || text === 'C' || text === 'D') {
-                upload(sender)
-                continue
-            }
-            sendTextMessage(sender, "Bot: " + text.substring(0, 200))
-        }
-        if (event.postback) {
-            text = JSON.stringify(event.postback)
-            sendTextMessage(sender, "Postback received: " + text.substring(0, 200), token)
-            continue
-        }
-    }
-res.sendStatus(200)
 })
 
 var token = "EAAR7rXLj81wBAEJmS62ZBE5stLHoeU0utxZAPnINOtXINLk6y2qvPprPSr24PYky5295bsNezPMIvF8xVIlGPQ0ZACQhiAbKt6MlzUZBoiZAE18bZBagDjzfXfZCPuv5Gylaaxzmp4MDm4wjdWRnupkcfqTjfh35AwKZA785ERJfVAZDZD"
@@ -237,10 +216,6 @@ request('https://qrackajack.expeditedaddons.com/?api_key=4956C3FYQSJB6PO70V9D0UA
   console.log('Headers:', JSON.stringify(response.headers));
   console.log('Response:', body);
 });
-
-function upload(file, options, callback)
-cloudinary.image("qrcode_piece_perdu.png")
-
 
 //zone test 
 
