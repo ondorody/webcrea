1322ce6260ee8b6b16655628cefc88daa72e6650
ondorody <37157793+ondorody@users.noreply.github.com>
2018-07-23 21:03:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,281 +1,84 @@
-// El Mehdi LAIDOUNI
+/**
+ * Copyright 2017-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ *
+ * Starter Project for Messenger Platform Quick Start Tutorial
+ *
+ * Use this project as the starting point for following the 
+ * Messenger Platform quick start tutorial.
+ *
+ * https://developers.facebook.com/docs/messenger-platform/getting-started/quick-start/
+ *
+ */
 
-var express = require('express')
-var bodyParser = require('body-parser')
-var request = require('request')
-var app = express()
+'use strict';
 
-app.set('port', (process.env.PORT || 5000))
+// Imports dependencies and set up http server
+const 
+  request = require('request'),
+  express = require('express'),
+  body_parser = require('body-parser'),
+  app = express().use(body_parser.json()); // creates express http server
 
-// Process urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
-// Process json
-app.use(bodyParser.json())
+// Accepts POST requests at /webhook endpoint
+app.post('/webhook', (req, res) => {  
 
-// Index route
-app.get('/', function (req, res) {
-    res.send('Bonjour, Je suis votre Facebook Messenger Bot')
-})
+  // Parse the request body from the POST
+  let body = req.body;
 
-app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'Bot_Messenger_App') {
-        res.send(req.query['hub.challenge'])
-    }
-    res.send('Error, wrong token')
-})
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
 
-app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
-})
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
 
+      // Get the webhook event. entry.messaging is an array, but 
+      // will only ever contain one event, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+      
+    });
 
-// End Point
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
 
-app.post('/webhook/', function (req, res) {
-    messaging_events = req.body.entry[0].messaging
-    for (i = 0; i < messaging_events.length; i++) {
-        event = req.body.entry[0].messaging[i]
-        sender = event.sender.id
-        if (event.message && event.message.text) {
-            text = event.message.text
-            if (text === 'Hello' || text === 'Hi' || text === 'QRcode' || text === 'generate') {
-                sendGenericMessage(sender)
-                continue
-            }
-            sendTextMessage(sender, "Bot: " + text.substring(0, 200))
-        }
-        if (event.postback) {
-            text = JSON.stringify(event.postback)
-            sendTextMessage(sender, "Postback received: " + text.substring(0, 200), token)
-            continue
-        }
-    }
-    res.sendStatus(200)
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
 
-    messaging_events = req.body.entry[0].messaging
-    for (i = 0; i < messaging_events.length; i++) {
-        event = req.body.entry[0].messaging[i]
-        sender = event.sender.id
-        if (event.message && event.message.text) {
-            text = event.message.text
-            if (text === 'geo' || text === 'Ouagadougou' || text === 'registration' || text === 'form') {
-                sendGenericMessaoge(sender)
-                continue
-            }
-            sendTextMessage(sender, "Bot: " + text.substring(0, 200))
-        }
-        if (event.postback) {
-            text = JSON.stringify(event.postback)
-            sendTextMessage(sender, "Postback received: " + text.substring(0, 200), token)
-            continue
-        }
-    }
-    res.sendStatus(200)
-})
-
-var token = "EAAR7rXLj81wBAJ99zHQTjUiprkvtoWH4gyz9NeN6mpXC11x5FhmaZB4uquDk3DfCGvGbcVoWwlhM1da6Ws8nqxZBrxeriPBuCPkvvPWpgpqp3hprNBudRZAMdZBTXLBfFVGsMjG5gU1XWnOas4LNxGO0ZAqSymdXangRlg5tbCfaN8eDFMbnd"
-
-// Echo back messages
-
-function sendTextMessage(sender, text) {
-    messageData = {
-        text:text
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
-
-// Two cards.
-
-function sendGenericMessage(sender) {
-   let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "SUBSCRIBE",
-                    "subtitle": "SUBSCRIBE",
-                    "image_url": "https://cdn.pixabay.com/photo/2015/09/19/17/02/map-947471_960_720.jpg",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://mbyan.heroku.com/",
-                        "title": "SUBSCRIBE"
-
-                    }, {
-                        "type": "web_url",
-                        "url": "https://mbyan.heroku.com/",
-                        "title": "SUBSCRIBE"
-                    }, {
-                        "type": "web_url",
-                        "url": "https://mbyan.heroku.com/",
-                        "title": "SUBSCRIBE"
-                    }],
-                }, {
-                    "title": "SUBSCRIBE",
-                    "subtitle": "SUBSCRIBE",
-                    "image_url": "https://cdn.pixabay.com/photo/2015/09/19/17/02/map-947471_960_720.jpg",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "SUBSCRIBE",
-                        "payload": "SUBSCRIBE",
-                    }, {
-                        "type": "postback",
-                        "title": "SUBSCRIBE",
-                        "payload": "SUBSCRIBE",
-                    }, {
-                        "type": "postback",
-                        "title": "SUBSCRIBE",
-                        "payload": "SUBSCRIBE",
-                    }],
-                }, {
-                    "title": "SUBSCRIBE",
-                    "subtitle": "SUBSCRIBE",
-                    "image_url": "https://cdn.pixabay.com/photo/2015/09/19/17/02/map-947471_960_720.jpg",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://mbyan.heroku.com/",
-                        "title": "SUBSCRIBE",
-
-                    }, {
-                        "type": "web_url",
-                        "url": "https://mbyan.heroku.com/",
-                        "title": "SUBSCRIBE",
-                    }, {
-                        "type": "web_url",
-                        "url": "https://mbyan.heroku.com/",
-                        "title": "SUBSCRIBE",
-                    }],
-
-                }, {
-                    "title": "SUBSCRIBE",
-                    "subtitle": "SUBSCRIBE",
-                    "image_url": "https://cdn.pixabay.com/photo/2015/09/19/17/02/map-947471_960_720.jpg",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "SUBSCRIBE",
-                        "payload": "SUBSCRIBE",
-                    }, {
-                        "type": "postback",
-                        "title": "SUBSCRIBE",
-                        "payload": "SUBSCRIBE",
-                        }, {
-                            "type": "postback",
-                            "title": "SUBSCRIBE",
-                            "payload": "SUBSCRIBE",
-                   
-                    }],
-                }]
-            }
-       }
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
-var request = require('request');
-
-request('https://iptoearth.expeditedaddons.com/?api_key=HTBCSM05UX6Q07389DL51JPN2FAVZ847EY3OR2G6WI1K94&ip=68.10.149.45', function (error, response, body) {
-    console.log('Status:', response.statusCode);
-    console.log('Headers:', JSON.stringify(response.headers));
-    console.log('Response:', body);
 });
 
-//zone test 
-
-
-function sendTextMessaoge(sender, text) {
-    let messageData = { text: text }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: token },
-        method: 'POST',
-        json: {
-            recipient: { id: sender },
-            message: messageData,
-        }
-    }, function (error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
-function sendGenericMessaoge(sender) {
-    let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "ADD PICTURES",
-                    "subtitle": "ADD PICTURES",
-                    "image_url": "https://cdn.pixabay.com/photo/2015/09/19/17/02/map-947471_960_720.jpg",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://www.facebook.com/groups/248598409284666/",
-                        "title": "Visit the Group"
-                    }, {
-                        "type": "postback",
-                        "title": "ADD PICTURES",
-                        "payload": "ADD PICTURES",
-                    }],
-                }, {
-                    "title": "ADD PICTURES",
-                    "subtitle": "ADD PICTURES",
-                    "image_url": "https://cdn.pixabay.com/photo/2015/09/19/17/02/map-947471_960_720.jpg",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "ADD PICTURES",
-                        "payload": "ADD PICTURES",
-                    }],
-                }]
-            }
-        }
+// Accepts GET requests at the /webhook endpoint
+app.get('/webhook', (req, res) => {
+  
+  /** UPDATE YOUR VERIFY TOKEN **/
+  const VERIFY_TOKEN = "EAAR7rXLj81wBAJ99zHQTjUiprkvtoWH4gyz9NeN6mpXC11x5FhmaZB4uquDk3DfCGvGbcVoWwlhM1da6Ws8nqxZBrxeriPBuCPkvvPWpgpqp3hprNBudRZAMdZBTXLBfFVGsMjG5gU1XWnOas4LNxGO0ZAqSymdXangRlg5tbCfaN8eDFMbnd";
+  
+  // Parse params from the webhook verification request
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  // Check if a token and mode were sent
+  if (mode && token) {
+  
+    // Check the mode and token sent are correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Respond with 200 OK and challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
     }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: token },
-        method: 'POST',
-        json: {
-            recipient: { id: sender },
-            message: messageData,
-        }
-    }, function (error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
+  }
+});
